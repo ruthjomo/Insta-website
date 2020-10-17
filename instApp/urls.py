2e5f89abb6a715fb3c41ce6bda810a1cc95a59ee
ruthjomo <ruthjomo19@gmail.com>
2020-10-17 12:27:33 +0300

    added files and installed pillow

--- instApp/urls.py
+++ instApp/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-     url('^signup/$',views.signup, name='signup'),
+    # url('^$',views.welcome,name = 'welcome'),
+     url('^$',views.signup, name='signup'),
     
 ]
--- instApp/views.py
+++ instApp/views.py
@@ -2,14 +2,18 @@ from django.contrib.auth import login, authenticate
 from . forms import  SignUpForm
 from django.shortcuts import render ,redirect
 from django.http  import HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView
+import datetime as dt
 
 
 
 
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to Instagram website')
+# def welcome(request):
+#     return HttpResponse('Welcome to Instagram website')
 
 def signup(request):
     if request.method == 'POST':
@@ -23,6 +27,20 @@ def signup(request):
             return redirect('home')
     else:
         form = SignUpForm()
-    return render(request, 'signup.html', {'form': form})    
+    return render(request, 'signup.html', {'form': form})  
+
+def get_object(self):
+        id_ = self.kwargs.get("username")
+        user = get_object_or_404(User, username=id_)
+        posts = user.posts.all()
+        return user
+
+def get_context_data(self, *args, **kwargs):
+    context = super(Profile,self).get_context_data(*args, **kwargs)
+    user = self.get_object()
+    context.update({
+    'posts' :          user.posts.all().filter(created_date__lte=timezone.now()).order_by(' -created_date')
+ })
+    return context      
 
 
