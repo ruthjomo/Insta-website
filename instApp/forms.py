aa98f077a954c17e0dcf201d1a1926be648298f2
ruthjomo <ruthjomo19@gmail.com>
2020-10-19 08:31:13 +0300

    installed crispy_forms

--- instApp/forms.py
+++ instApp/forms.py
@@ -1,2 +1,18 @@
 from django import forms
+from .models import Post
+from crispy_forms.helper import formhelper
+from crispy_forms.layout import submit,Layout,field
 
+class PostForm(forms.modelform):
+      helper = formhelper()
+      helper.form_method = 'POST'
+      helper.add_input(submit('post','post',css_class='btn-primary'))
+
+      class meta:
+          model = post
+          fields = [
+              'image',
+              'caption'
+          ]
+
+  
