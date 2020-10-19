f6286a36a1f5fba7f40380b03e0f596e928f8422
ruthjomo <ruthjomo19@gmail.com>
2020-10-19 06:55:22 +0300

    added more changes in the file

--- myInsta/urls.py
+++ myInsta/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,8 +24,10 @@ urlpatterns = [
 
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url('admin/', admin.site.urls),
     url('instApp/',include('instApp.urls'))
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
