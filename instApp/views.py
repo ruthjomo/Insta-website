b9f7c21b59e6a11650dda72ba953cff3d6148238
ruthjomo <ruthjomo19@gmail.com>
2020-10-19 09:07:19 +0300

    added changes in the file

--- instApp/views.py
+++ instApp/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+from .forms import PostForm
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView,CreateView
 # Create your views here.
 
 class PostListView(ListView):
     template_name = "post_list.html"
     queryset = Post.objects.all()
     context_object_name = "posts"
+
+class PostCreateView(CreateView) :
+    template = "/post_create.html" 
+    from_class = PostForm 
+    queryset = post.objects.all()
+
+    def form_valid(self,form):
+        print(form.cleaned_data)
+        #To prevent user from inputting their name every time they want to make a post
+        form.instance.author = self.request.user
+        return super().form_valid(form)
